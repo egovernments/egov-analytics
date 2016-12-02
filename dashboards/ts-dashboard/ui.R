2a2f19dfd3bf4524194841a62b947266f059dc62
samarthbhargav <samarth.bhargav92@gmail.com>
2016-12-02 16:51:56 +0530

    [TS-Dashboard] Added additional controls, removed UCM and SARIMA

--- dashboards/ts-dashboard/ui.R
+++ dashboards/ts-dashboard/ui.R
@@ -4,43 +4,57 @@ shinyUI(fluidPage(
   sidebarLayout(
     sidebarPanel(
       width = 4,
-      fluidRow(
-        selectInput(
-          "dataSet", label = h5("Data Set"),
-          choices = c(names(datasets))
+      tabsetPanel(
+        tabPanel(
+          title = 'Data and pre-processing',
+          fluidRow(
+            selectInput(
+              "dataSet", label = h5("Data Set"),
+              choices = c(names(datasets))
+            )
+          ), 
+          fluidRow(
+            h5("Data Preprocessing Controls"),
+            checkboxInput("cleanOutliers", "Remove Outliers", value = F),
+            checkboxInput("STL", "STL", value = F),
+            sliderInput("STL.Window", label="STL Window (0=periodic)", min=0, max=12, value = 0)
+          )
+        ), tabPanel(
+          title = "Forecasting Method",
+          fluidRow(
+            selectInput(
+              "method", label = h5("Modeling Method"),
+              choices = c("ARIMA", 
+                          "Exponential_Smoothing_Standard",
+                          "Exponential_Smoothing_HoltWinters")
+            )
+          ), fluidRow(
+            h3("ARIMA Parameters"),
+            sliderInput("ARIMA.p", label="p", min=1, max=18, value = 1),
+            sliderInput("ARIMA.d", label="d", min=0, max=12, value = 0),
+            sliderInput("ARIMA.q", label="q", min=1, max=18, value = 1)
+          )
         )
-      ), 
-      fluidRow(
-        selectInput(
-          "method", label = h5("Modeling Method"),
-          choices = c("ARIMA", 
-                      "Exponential_Smoothing_Standard",
-                      "Exponential_Smoothing_HoltWinters",
-                      "UCM")
-        )
-      ),
-      fluidRow(
-        h3("ARIMA Parameters"),
-        checkboxInput("ARIMA.boxCox", "Apply Box-Cox Transformation", value=F),
-        checkboxInput("ARIMA.seasonal", "SARIMA", value=F),
-        sliderInput("ARIMA.lambda", label="Lambda(Box-Cox Transformation)", min=0.0, max=1, step=.01, value=0),
-        sliderInput("ARIMA.p", label="p", min=1, max=18, value = 1),
-        sliderInput("ARIMA.d", label="d", min=0, max=12, value = 0),
-        sliderInput("ARIMA.q", label="q", min=1, max=18, value = 1),
-        sliderInput("ARIMA.P", label="P (seasonal)", min=1, max=18, value = 0),
-        sliderInput("ARIMA.D", label="D (seasonal)", min=0, max=12, value = 0),
-        sliderInput("ARIMA.Q", label="Q (seasonal)", min=1, max=18, value = 0),
-        # params for UCM
-        h3("UCM Parameters"),
-        sliderInput("UCM.season.length", label="Season Length", min=5, max=18, value = 6),
-        sliderInput("UCM.cycle.period", label="Cycle Period", min=1, max=12, value = 6)
       )
     ),
     mainPanel(
-      plotOutput("plotForecast"),
-      tableOutput("displayMetrics"),
-      plotOutput("plotRaw"),
-      plotOutput("plotTs")
+      tabsetPanel(
+        tabPanel(
+          title = 'Data Pre-processing',
+          tags$style(type = "text/css",
+                     ".shiny-output-error { visibility: hidden; }",
+                     ".shiny-output-error:before { visibility: hidden; }"
+          ),
+          plotOutput("plotOutliers"),
+          plotOutput("plotSTL")
+        ),
+        tabPanel(
+          title = 'Forecasts / Results',
+          plotOutput("plotTs"),
+          plotOutput("plotForecast"),
+          tableOutput("displayMetrics")
+        )
+      )
     )
   )
 ))
