66b33597f2fd45dabf3028620536527090c28c7c
samarthbhargav <samarth.bhargav92@gmail.com>
2016-12-22 12:33:03 +0530

    [Alerts] Exploratory work on alerts. Updated dash with work

--- dashboards/alerts-dashboard/ui.R
+++ dashboards/alerts-dashboard/ui.R
@@ -23,18 +23,31 @@ shinyUI(fluidPage(
         dateRangeInput(
           "dateRange", label = "Date Range",
           start = as.Date(minDate), end = as.Date(maxDate)
+        ),
+        checkboxInput(
+          "detectAnoms", label = "Detect Anomalies?"
+        ),
+        dateInput(
+          "anoms.date", label = "Date to detect anomalies around",
+          value = "2014-11-22"
+        ),
+        sliderInput(
+          "window.size", label = "Days to consider",
+          min = 30, max = 120, value = 60
         )
       )
     ),
     mainPanel(
       tabsetPanel(
         tabPanel(
-          title = 'Trends',
+          title = 'Data',
           tags$style(type = "text/css",
                      ".shiny-output-error { visibility: hidden; }",
                      ".shiny-output-error:before { visibility: hidden; }"
           ),
-          dygraphOutput('plotData')
+          dygraphOutput('plotData'),
+          h4("Output of AnomalyDetection (only last day)"),
+          plotOutput('plotAnoms')
         )
       )
     )
