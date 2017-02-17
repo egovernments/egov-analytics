504ca757cbe0e503f0fb3d56697f3e4a77208d26
samarthbhargav <samarth.bhargav92@gmail.com>
2017-02-17 23:03:36 +0530

    Added anom exec + final JSON output

--- eGovs.lib/R/anoms.R
+++ eGovs.lib/R/anoms.R
@@ -8,18 +8,35 @@ construct.frame <- function(xtsobj) {
 #' @param series, a time-series
 #' @param start_date (optional) the start date to consider when detecting anoms
 #' @param end_date (optional) the end date to consider when detecting anoms
+#' @param model.alpha (optional) alpha param to pass to the algorithm
+#' @param model.max_anoms (optional) max_anoms param to pass to the algorithm
 #' @export
-detect_anomalies = function(series, start_date=NULL, end_date=NULL) {
+detect_anomalies = function(series,
+                            start_date=NULL,
+                            end_date=NULL,
+                            model.alpha = 0.05,
+                            model.max_anoms = 0.1) {
   if(!is.null(start_date)) {
-    series <- window(series, start= paste0(start_date," 00:00:00"))
+    series <- window(series, start=paste0(start_date," 00:00:00"))
   }
   if(!is.null(end_date)) {
-    series <- window(series, end= paste0(end_date," 23:59:59"))
+    series <- window(series, end=paste0(end_date," 23:59:59"))
+  }
+  anomalies <- AnomalyDetection::AnomalyDetectionVec(drop(zoo::coredata(series)),
+                                                     period=24, plot=F,
+                                                     direction = "pos",
+                                                     alpha = model.alpha,
+                                                     max_anoms = model.max_anoms)
+  if(nrow(anomalies$anoms) == 0) {
+    warning("No anomalies found!", immediate. = T)
+    return(NULL)
+  } else {
+    out_subset <- data.frame(Count = series[anomalies$anoms$index,])
+    out_subset$Time <- row.names(out_subset)
+    row.names(out_subset) <- NULL
   }
 
-  series <- construct.frame(series)
-  anomaly <- AnomalyDetectionTs(series, plot=F)
-  anomaly$anoms
+  return(out_subset)
 }
 
 #' Detect anomalies in the date specified
@@ -49,7 +66,7 @@ detect_anomalies_last <- function(series,
                                                      alpha = model.alpha,
                                                      max_anoms = model.max_anoms)
   if(nrow(anomalies$anoms) == 0) {
-    warning("No anomalies found!")
+    warning("No anomalies found!", immediate. = T)
     return(NULL)
   } else {
     out_subset <- data.frame(Count = series[anomalies$anoms$index,])
--- eGovs.lib/R/config.R
+++ eGovs.lib/R/config.R
@@ -6,7 +6,46 @@ Constants <- list(
 )
 
 
-Config <- R6Class(
+AlertsConfig <- R6Class(
+  "AlertsConfig",
+  public = list(
+    config = NULL,
+    params = NULL,
+    initialize = function(config.path) {
+      self$config <- suppressWarnings(jsonlite::fromJSON(readLines(config.path), flatten = FALSE))
+      self$params <- c("alpha", "max_anoms")
+    },
+    getCityLevelConfig = function() {
+      self$config[["alerts"]][["city_level"]]
+    },
+    getDefaultWardLevelConfig = function() {
+      self$config[["alerts"]][["ward_level"]][["defaults"]]
+    },
+    getDefaultComplaintLevelConfig = function() {
+      self$config[["alerts"]][["complaint_level"]][["defaults"]]
+    },
+    getDataPath = function() {
+      self$config[["data"]][["path"]]
+    },
+    getComplaintLevelConfig = function(complaint.type) {
+      c <- self$config[["alerts"]][["complaint_level"]][["complaints"]]
+      if(complaint.type %in% names(c)) {
+        return(c[[complaint.type]])
+      }
+      self$getDefaultComplaintLevelConfig()
+    },
+    getWardLevelConfig = function(ward) {
+      c <- self$config[["alerts"]][["ward_level"]][["wards"]]
+      if(ward %in% names(c)) {
+        return(c[[ward]])
+      }
+      self$getDefaultWardLevelConfig()
+    }
+  )
+)
+
+
+ForecastsConfig <- R6Class(
   "ForecastsConfig",
   public = list(
     config = NULL,
@@ -17,13 +56,13 @@ Config <- R6Class(
       self$config[["data"]][["path"]]
     },
     getModelSpec = function(complaint.type) {
-      model_spec <- self$config[["model-spec"]][[complaint.type]]
+      model_spec <- self$config[["forecasts"]][["model-spec"]][[complaint.type]]
       if(is.null(model_spec)) {
         stop(paste("Model spec is not present for complaint type:", complaint.type))
       }
       model_spec
     }, getComplaintTypesToModel = function() {
-      names(self$config[["model-spec"]])
+      names(self$config[["forecasts"]][["model-spec"]])
     },
     getModelType = function(complaint.type) {
       self$getModelSpec(complaint.type)[["model"]]
